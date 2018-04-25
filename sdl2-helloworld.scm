11bc2a22c6a7ad0882379bf23806846cd8bf9f7f
Pierre Neidhardt <ambrevar@gmail.com>
2018-04-25 20:00:37 +0530

    sdl2-helloworld: Use load-image for PNG support

--- sdl2-helloworld.scm
+++ sdl2-helloworld.scm
@@ -1,20 +1,34 @@
+#!guile \
+-e main -s
+!#
+
+;; TODO: Add prefix.
 (use-modules (sdl2)
              (sdl2 render)
+             (sdl2 image)
              (sdl2 surface)
              (sdl2 video))
 
 (define (draw ren)
-  (let* ((surface (load-bmp "textures/background.bmp"))
+  (let* ((surface (load-image "textures/background.png"))
          (texture (surface->texture ren surface)))
+    ;; TODO: Why do we specify "ren" twice?
     (clear-renderer ren)
     (render-copy ren texture)
     (present-renderer ren)
     (sleep 2)))
 
-(sdl-init)
+;; ;; TODO: Use OS separator.
+;; (define player (SDL:load-image (string-append image-dir "/character.png")))
+;; (define player-pos (SDL:make-rect 0 0 32 32))
+;; (define* (move-player #:optional (distance 1))
+;;   (SDL:rect:set-x! player-pos (+ (SDL:rect:x player-pos) distance))
+;;   (SDL:blit-surface player #f (SDL:get-video-surface) player-pos))
 
-(call-with-window (make-window)
-  (lambda (window)
-    (call-with-renderer (make-renderer window) draw)))
+(define (main)
+  (sdl-init)
+  (call-with-window (make-window)
+                    (lambda (window)
+                      (call-with-renderer (make-renderer window) draw)))
 
-(sdl-quit)
+  (sdl-quit))
