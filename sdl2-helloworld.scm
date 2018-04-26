af79ed76317d28dc492274ca9c005e3285af7af3
Pierre Neidhardt <ambrevar@gmail.com>
2018-04-26 14:16:26 +0530

    sdl2-helloworld: Set window size

--- sdl2-helloworld.scm
+++ sdl2-helloworld.scm
@@ -2,6 +2,9 @@
 -e main -s
 !#
 
+;; TODO: Size background properly when windowed: looks like we need to load
+;; background first and get its dimensions.
+
 (use-modules (sdl2)
              ((sdl2 render) #:prefix sdl:)
              ((sdl2 image) #:prefix sdl:)
@@ -11,7 +14,9 @@
 (define (draw ren)
   (let* ((surface (sdl:load-image "textures/background.png"))
          (texture (sdl:surface->texture ren surface)))
-    ;; TODO: Why do we specify "ren" twice?
+    ;; TODO: Can we get window from renderer?
+    ;; (sdl:set-window-size! window (list (sdl:surface-width surface) (sdl:surface-height surface)))
+    ;; TODO: Why do we specify "ren" in surface->texture?  Maybe because of hardware specificities.
     (sdl:clear-renderer ren)
     (sdl:render-copy ren texture)
     (sdl:present-renderer ren)
@@ -24,9 +29,13 @@
 ;;   (SDL:rect:set-x! player-pos (+ (SDL:rect:x player-pos) distance))
 ;;   (SDL:blit-surface player #f (SDL:get-video-surface) player-pos))
 
+;; TODO: repl + running?
+;; TODO: Options (fps)
+
 (define* (main #:optional args)
   (sdl-init)
-  (sdl:call-with-window (sdl:make-window)
+  (sdl:call-with-window (sdl:make-window ;; #:fullscreen-desktop? #t
+                                         #:size '(460 460))
                         (lambda (window)
                           (sdl:call-with-renderer (sdl:make-renderer window) draw)))
   (sdl-quit))
